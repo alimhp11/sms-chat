e1986f589a73a6252b68bf39acbbfdbd3bc7605d
ali <alimohammadi457890@gmail.com>
2026-07-04 15:38:30 +0330

    url edited

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lobby, name='lobby'),
-    path('hello/', views.hello, name='hello'),
+    path('hello/h', views.hello, name='hello'),
     path('register/', views.register_view, name='register'),
     path('create-room/', views.create_room, name='create_room'),
     path('chat/<str:room_name>/', views.room, name='room'),
